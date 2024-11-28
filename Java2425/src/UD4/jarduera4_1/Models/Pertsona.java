a85145b8832bf7e3e34ce9276516498f01064c58
Xabier <x_bilbaozam@fpzornotza.com>
2024-11-28 13:55:06 +0100

    Year 2024 Week 48 Day 4

---
+++ Java2425/src/UD4/jarduera4_1/Models/Pertsona.java
@@ -0,0 +1,54 @@
+package UD4.jarduera4_1.Models;
+
+public class Pertsona {
+	
+	// ATTRIBUTES
+	
+	private String izena;
+	private int adina;
+	private String dni;
+	
+	
+	// CONSTRUCTORS
+	
+	public Pertsona() {
+	
+	}
+	
+	
+	
+	
+	
+	// GETTERS
+	
+	public String getIzena() {
+		return izena;
+	}
+	
+	public int getAdina() {
+		return adina;
+	}
+	
+	public String getDni() {
+		return dni;
+	}
+	
+	
+	
+	
+	
+	// SETTERS
+	
+	public void setIzena(String izena) {
+		this.izena = izena;
+	}
+	
+	public void setAdina(int adina) {
+		this.adina = adina;
+	}
+	
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+}
---
+++ Java2425/src/UD4/jarduera4_1/PertsonaAPP.java
@@ -0,0 +1,27 @@
+package UD4.jarduera4_1;
+
+import java.util.ArrayList;
+
+// IMPORTED MODELS //
+
+import UD4.jarduera4_1.Models.Pertsona;
+
+public class PertsonaAPP {
+
+	public static void main(String[] args) {
+		
+		// VARIABLES //
+		
+			// CREATING THE ARRAYLIST FOR THE CLASS "PERTSONA" //
+		
+		ArrayList <Pertsona> pertsonaList = new ArrayList<Pertsona>();
+		
+			// OTHER VARIABLES //
+		
+		
+		// PROGRAM //
+		
+		
+	}
+
+}
