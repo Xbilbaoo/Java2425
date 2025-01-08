d523b926da9d6b81933090b794682500ff4d813a
Xabier <x_bilbaozam@fpzornotza.com>
2025-01-08 13:56:31 +0100

    Year 2025 Week 2 Day 3

--- Java2425/src/UD4/jarduera4_10/IbilgailuAPP.java
+++ Java2425/src/UD4/jarduera4_10/IbilgailuAPP.java
@@ -6,9 +6,12 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import UD4.jarduera4_10.models.Color;
+
 // IMPORTED MODELS //
 
 import UD4.jarduera4_10.models.Ibilgailua;
+import UD4.jarduera4_10.models.Taxi;
 
 public class IbilgailuAPP {
 
@@ -16,8 +19,12 @@ public class IbilgailuAPP {
 		
 		// VARIABLES //
 		
-		File file = new File("./files/UD4/ibilgailuak.csv");
-		Scanner scFile = new Scanner(file);
+		File file1 = new File("./files/UD4/410/ibilgailuak.csv");
+		File file2 = new File("./files/UD4/410/Taxi.csv");
+		File file3 = new File("./files/UD4/410/autobusak.csv");
+		Scanner scFile1 = new Scanner(file1);
+		Scanner scFile2 = new Scanner(file2);
+		
 		Scanner sc = new Scanner(System.in);
 		int aukera = 0;
 		
@@ -40,15 +47,15 @@ public class IbilgailuAPP {
 			
 			case 1:
 				
-				
+				taxiaSortu(sc, ibilgailuList);
 			}
 			
 		} while(aukera != 0);
 	}
 
-	
-	
-	
+
+
+
 
 	// METHODS //
 		
@@ -66,6 +73,42 @@ public class IbilgailuAPP {
 		System.out.println(" 3.- KOTXE BATEN DATUAK SARTU.");
 		System.out.println(" 4.- TAXI BATEN LIZENTZIA ZENBAKIA IKUSTEA.");
 		System.out.println(" 5.- AUTOBUS GUZTIEN PLAZA KOPURUA IKUSI.");
+		System.out.println(" 6.- DATUAK GORDE.");
+		System.out.println(" 0.- IRTEN.");
+		
+	}
+	
+		// METHOD TO CREATE AN INSTANCE OF A TAXI //
+	
+	private static void taxiaSortu(Scanner sc, ArrayList<Ibilgailua> ibilgailuList) {
+		
+		// CREATE A NEW INTANCE OF THE CLASS TAXI //
+		
+		Taxi taxi = new Taxi();
+		
+		// INSERT THE VALUES OF THE INSTANCE VIA KEYBOARD //
+		
+		System.out.println("Idatzi taxiaren lizentzia zebakia.");
+		taxi.setLizentziaZenbakia(Integer.parseInt(sc.nextLine()));
+		
+		System.out.println("Idatzi taxiaren matrikula.");
+		taxi.setMatrikula(sc.nextLine());
+		
+		System.out.println("Idatzi taxiaren modeloa");
+		taxi.setModeloa(sc.nextLine());
+		
+		System.out.println("Idatzi taxiaren plaza kopurua.");
+		taxi.setPlazaKopurua(Integer.parseInt(sc.nextLine()));
+		
+		System.out.println("Idatzi taxiak duen potentzia.");
+		taxi.setPotentzia(Integer.parseInt(sc.nextLine()));
+		
+		System.out.println("Idatzi taxiaren kolorea (ZURIA, BELTZA, URDINA)");
+		taxi.setKolorea(Color.valueOf(sc.nextLine().toUpperCase()));
+		
+		// ADD THE INTANCE TO THE ARRAYLIST //
+		
+		ibilgailuList.add(taxi);
 		
 	}
 }
--- Java2425/src/UD4/jarduera4_10/models/Taxi.java
+++ Java2425/src/UD4/jarduera4_10/models/Taxi.java
@@ -1,20 +1,55 @@
 package UD4.jarduera4_10.models;
 
 public class Taxi extends Ibilgailua {
-	
+
 	// ATTRIBUTES //
-	
+
 	private int lizentziaZenbakia;
 
-	
-	
 	// CONSTRUCTORS //
-	
-	public Taxi(int lizentziaZenbakia) {
-		
-		super();
-		this.lizentziaZenbakia = lizentziaZenbakia;
-		
+
+	public Taxi() {
+
 	}
 
+	public Taxi(int lizentziaZenbakia) {
+
+		super();
+		this.lizentziaZenbakia = lizentziaZenbakia;
+
+	}
+
+	// GETTERS //
+
+	public int getLizentziaZenbakia() {
+
+		return lizentziaZenbakia;
+
+	}
+
+	// SETTERS //
+
+	public void setLizentziaZenbakia(int lizentziaZenbakia) {
+
+		this.lizentziaZenbakia = lizentziaZenbakia;
+
+	}
+
+	// TO STRING //
+
+	public String toString() {
+
+		return "Taxi [lizentziaZenbakia=" + lizentziaZenbakia + "]";
+
+	}
+
+	// METHODS //
+
+	// METHOD TO SAVE EACH INSTANCE ON CSV LINE //
+
+	public String saveData() {
+
+		return this.getMatrikula() + ";" + this.getModeloa() + ";" + this.getPotentzia() + ";" + this.getPlazaKopurua()
+				+ ";" + this.getKolorea() + ";" + lizentziaZenbakia;
+	}
 }
